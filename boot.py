2722298752f44da95ede809a619822e0614ff673
Daniele Lupo <daniele.lupo@riatlas.it>
2022-01-11 00:10:25 +0100

    Vari
     - Avviato il server FTP all'avvio poiché Thonny crasha di tanto in tanto;
     - Impostato l'intervallo di controllo a 30 minuti;
     - Settati data e ora odierni all'avvio (supporta solo UTC+0).

--- boot.py
+++ boot.py
@@ -3,10 +3,14 @@
 #esp.osdebug(None)
 import wifiManager as myWifiManager
 import logManager as lm
+import network
 #import webrepl
 
 connesso = myWifiManager.connettiWifiVodafone()
 if connesso:
+    # Recupero data e ora dal server ntp
+    import ntptime
+    ntptime.settime()
     lm.scriviLog("Connesso al wifi.")
     #webrepl.start()
     import modemManager as mm
@@ -16,10 +20,15 @@ if connesso:
     def avviaThreadLoopCheck():
         im.inviaNotifica(testo = "Avviato loopCheck")
         return t.start_new_thread(mm.loopCheck, ())
+    
+    def avviaFTP():
+        import uftpd as serverFTP
+        wifi = network.WLAN(network.STA_IF)
+        im.inviaNotifica(testo = "IP {}".format(wifi.ifconfig()[0]))
 
-    avviaThreadLoopCheck()
     im.inviaNotifica(testo = "ESP32 avviato")
+    avviaThreadLoopCheck()
+    avviaFTP()
+    
 else:
     lm.scriviLog("Impossibile connettersi.")
-
-
